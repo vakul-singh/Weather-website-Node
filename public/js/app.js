706ca3e99e27885f24e99f7557999968682d8f98
Vakul <vakul.12@gmail.com>
2020-02-06 01:55:56 +0530

    Final changes made

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,29 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne= document.querySelector('#message-1')
-const messageTwo= document.querySelector('#message-2')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 messageOne.textContent = ''
 
-// weatherForm.addEventListner('submit',())
-//e=event
-weatherForm.addEventListener('submit' , (e) => {
+
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const location=search.value
+    const location = search.value
 
-    messageOne.textContent="Loading...."
-    messageTwo.textContent=""
+    messageOne.textContent = "Loading...."
+    messageTwo.textContent = ""
 
-    fetch('/weather?address='+location).then((response) =>{
-    //"then" function is going to run when the json data has arrived
-    response.json().then((data) => {
-        // console.log(data)
-        if(data.error)  {
-            messageOne.textContent = data.error
-        }  else  {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-            // console.log(data.location)
-            // console.log(data.forecast)
-        }
+    fetch('/weather?address=' + location).then((response) => {
+
+        response.json().then((data) => {
+
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
-})
 
 })
 
--- src/app.js
+++ src/app.js
@@ -1,81 +1,80 @@
 const path = require('path')
-const express=require('express')   
-const hbs= require('hbs')
-const geocode= require('./utils/geocode')
-const forecast= require('./utils/forecast')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const app=express()             
-const port=process.env.PORT || 3000
+const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for Express config
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlebar engine and views location
-app.set('view engine','hbs')        
-app.set('views',viewsPath)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
 //Setup static directory
 app.use(express.static(publicDirectoryPath))
 
-app.get('',(req,res) => {
-    res.render('index',{
-        title:'Weather Today',
-        name:'Vakul Singh'
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Today',
+        name: 'Vakul Singh'
     })
 })
 
-app.get('/about',(req,res) => {
-    res.render('about',{
-        title:'About me',
-        name:'Vakul Singh'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Vakul Singh'
     })
 })
 
-app.get('/help',(req,res) => {
-    res.render('help',{
-        helpText:'This is a website to get information about current weather.You just need to enter a location to get weather result. ',
-        title:'Help',
-        name:'Vakul Singh'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Vakul Singh'
     })
 })
 
 //res stringify object data itself to convert to JSON
-app.get('/weather',(req,res) => {
-    if(!req.query.address){
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
         return res.send({
-            error:'You must provide an address'
+            error: 'You must provide an address'
         })
     }
 
 
-    geocode(req.query.address,(error,{latitude,longitude,location} = {}) => {
-        if(error){
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
             return res.send({ error })
-        }   
-    
-        forecast(latitude, longitude ,(error,forecastData) =>{
-            if(error){
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
                 return res.send({ error: error })
-            }   
+            }
             res.send({
                 forecast: forecastData,
                 location: location,
-                address:   req.query.address
-                
-            })               
+                address: req.query.address
+
+            })
         })
     })
 })
 
 
-app.get('/help/*',(req,res) => {
-    res.render('404',{
-        title:'404',
-        name:'Vakul Singh',
-        errorMessage:'Help article not found'
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Vakul Singh',
+        errorMessage: 'Help article not found'
     })
 })
 
@@ -84,8 +83,8 @@ app.get('/help/*',(req,res) => {
 
 
 
-app.get('/products',(req,res) => {
-    if(!req.query.search){
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
         return res.send({
             error: 'you must provide a search term'
         })
@@ -93,22 +92,20 @@ app.get('/products',(req,res) => {
 
     console.log(req.query.search)
     res.send({
-        products:[]
+        products: []
     })
 })
 
-app.get('*',(req,res) => {
-    res.render('404',{
-        title:'404',
-        name:'Vakul Singh',
-        errorMessage:'page not found'
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Vakul Singh',
+        errorMessage: 'page not found'
     })
 })
-// app.com 
 
 // lets start the server up,process of starting a server is a asynchronous process
-
 app.listen(port, () => {
-    console .log('Server is up on port '+port)
+    console.log('Server is up on port ' + port)
 })
 
